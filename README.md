fec690bac4ef967ad18ce9c9efd62687e86f60c1
Morghan <morghan102@gmail.com>
2021-07-06 15:08:18 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # courseraOakBooks
+
+This is coursework from coursera: React Native - The Complete 2021 Guide with NodeJS & MongoDB
+You can find this course here:  https://www.udemy.com/course/the-complete-2021-react-native-guide-with-nodejs-mongodb/
---
+++ src/screens/BooksScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const BooksScreen = () => {
+ return (
+     <View>
+         <Text>Books screen</Text>
+     </View>
+ );   
+}
+
+const styles = StyleSheet.create({});
+
+export default BooksScreen;
